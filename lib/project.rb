d9a186b50ff09e7fc1a1d7485855113ce68d7132
Austin Powell <austinpowell58@gmail.com>
2018-08-21 22:55:20 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+  
+  attr_accessor :backers, :title
+  
+  def initialize(project_name)
+    @title = project_name
+    @backers = []
+  end 
+  
+  def add_backer(some_backer)
+    @backers << some_backer
+    some_backer.back_project(self)
+  end 
+    
+  
+end 
